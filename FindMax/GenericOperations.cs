ee6ab54a90ee2f2b28809254e0a705be3e4122eb
S Soundarya <soundarya.s@tvsnext.com>
2023-07-24 13:10:06 +0530

    [Soundarya]Add:Added GenericOperations.cs

---
+++ FindMax/GenericOperations.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMax
+{
+    public class GenericOperations
+    {
+
+        public T FindMax<T>(T a, T b, T c)
+        {
+            Comparer<T> comparer = Comparer<T>.Default;
+
+            T max = a;
+            if (comparer.Compare(a, b) >= 0 && comparer.Compare(a, c) >= 0)
+            {
+
+                max = a;
+                Console.WriteLine("a is max");
+            }
+            if (comparer.Compare(b, c) >= 0 && comparer.Compare(b, a) >= 0)
+            {
+
+                max = b;
+                Console.WriteLine("b is max");
+            }
+            if (comparer.Compare(c, a) >= 0 && comparer.Compare(c, b) >= 0)
+            {
+
+                max = c;
+                Console.WriteLine("c is max");
+            }
+            return max;
+        }
+    }
+}
--- FindMax/Program.cs
+++ FindMax/Program.cs
@@ -9,6 +9,12 @@ namespace FindMax
             operation.FindMaxInteger(1, 2, 3);
             operation.FindMaxFloat(1.2f, 1.6f, 1.1f);
             operation.FindMaxString("a", "b", "c");
+
+            GenericOperations genericOperation = new GenericOperations();
+            Console.WriteLine("\n\nGeneric:\n");
+            genericOperation.FindMax<int>(1, 2, 3);
+            genericOperation.FindMax<float>(1.1f, 1.6f, 1.4f);
+            genericOperation.FindMax<string>("a", "b", "c");
         }
     }
 }
