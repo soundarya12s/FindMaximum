92c7fa089c568a489afb49ea8811eb59e7a95349
S Soundarya <soundarya.s@tvsnext.com>
2023-07-24 15:57:44 +0530

    [Soundarya]Add:Update

--- FindMax/Program.cs
+++ FindMax/Program.cs
@@ -5,24 +5,60 @@ namespace FindMax
     {
         static void Main(string[] args)
         {
-            Operation operation = new Operation();
-            operation.FindMaxInteger(1, 2, 3);
-            operation.FindMaxFloat(1.2f, 1.6f, 1.1f);
-            operation.FindMaxString("a", "b", "c");
+            //Operation operation = new Operation();
+            //operation.FindMaxInteger(1, 2, 3);
+            //operation.FindMaxFloat(1.2f, 1.6f, 1.1f);
+            //operation.FindMaxString("a", "b", "c");
 
-      
-            int[] arr = { 5, 2, 3 ,4};
-            float[] Farr = { 1.2f, 1.8f, 2.0f,0.2f };
-            string[] Sarr= { "d", "b", "c","a" };
 
             GenericOperations genericOperation = new GenericOperations();
             Console.WriteLine("\n\nGeneric:\n");
 
-            
+            bool flag = true;
+           
+            Console.WriteLine("Enter the size of  array:");
+            int size = Convert.ToInt32(Console.ReadLine());
 
-            genericOperation.FindMax<int>(arr);
-            genericOperation.FindMax<float>(Farr);
-            genericOperation.FindMax<string>(Sarr);
+            Console.WriteLine("Enter the datatype:\n 1.Integer\n 2.Float\n 3.String\n 4.Exit");
+
+
+            while (flag)
+            {
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+
+                    case 1:
+                        int[] arr= new int[size];
+                        for(int i = 0; i < size; i++)
+                        {
+                            arr[i]= Convert.ToInt32(Console.ReadLine());
+                        }
+                        genericOperation.FindMax<int>(arr);
+                        break;
+                    case 2:
+                        float[] Farr = new float[size];
+                        for(int i=0; i < size; i++)
+                        {
+                            Farr[i]=Convert.ToSingle(Console.ReadLine());
+                        }
+                        genericOperation.FindMax<float>(Farr);
+                        break;
+                    case 3:
+                        string[] Sarr= new string[size];
+                        for(int i = 0; i < size; i++)
+                        {
+                            Sarr[i] = Console.ReadLine();
+                        }
+                        genericOperation.FindMax<string>(Sarr);
+                        break;
+                    case 4:
+                        flag = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
