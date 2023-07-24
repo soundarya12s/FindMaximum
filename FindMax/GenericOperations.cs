54b90eff0547adfa9db7e78926ce512fb8a5cfff
S Soundarya <soundarya.s@tvsnext.com>
2023-07-24 15:23:24 +0530

    [Soundarya]Add: Added array sorting

--- FindMax/GenericOperations.cs
+++ FindMax/GenericOperations.cs
@@ -8,12 +8,12 @@ namespace FindMax
 {
     public class GenericOperations
     {
-        public T FindMax<T>(T[] array)
+        public void FindMax<T>(T[] array)
         {
             Array.Sort(array);
            
             PrintMax(array.Max());
-            return array.Max();
+           
         }
         public void PrintMax<T>(T max)
         {
