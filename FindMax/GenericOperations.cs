cf717a0e83353a14c5182c32abddcbb2d34390e7
S Soundarya <soundarya.s@tvsnext.com>
2023-07-24 15:18:27 +0530

    [Soundarya]Add: Added array sorting

--- FindMax/GenericOperations.cs
+++ FindMax/GenericOperations.cs
@@ -8,30 +8,12 @@ namespace FindMax
 {
     public class GenericOperations
     {
-        public T FindMax<T>(T a, T b, T c)
+        public T FindMax<T>(T[] array)
         {
-            Comparer<T> comparer = Comparer<T>.Default;
-
-            T max = a;
-            if (comparer.Compare(a, b) >= 0 && comparer.Compare(a, c) >= 0)
-            {
-
-                max = a;
-                PrintMax(max);
-            }
-            if (comparer.Compare(b, c) >= 0 && comparer.Compare(b, a) >= 0)
-            {
-
-                max = b;
-                PrintMax(max);
-            }
-            if (comparer.Compare(c, a) >= 0 && comparer.Compare(c, b) >= 0)
-            {
-
-                max = c;
-                PrintMax(max);
-            }
-            return max;
+            Array.Sort(array);
+           
+            PrintMax(array.Max());
+            return array.Max();
         }
         public void PrintMax<T>(T max)
         {
--- FindMax/Program.cs
+++ FindMax/Program.cs
@@ -10,11 +10,19 @@ namespace FindMax
             operation.FindMaxFloat(1.2f, 1.6f, 1.1f);
             operation.FindMaxString("a", "b", "c");
 
+      
+            int[] arr = { 5, 2, 3 ,4};
+            float[] Farr = { 1.2f, 1.8f, 2.0f,0.2f };
+            string[] Sarr= { "d", "b", "c","a" };
+
             GenericOperations genericOperation = new GenericOperations();
             Console.WriteLine("\n\nGeneric:\n");
-            genericOperation.FindMax<int>(1, 2, 3);
-            genericOperation.FindMax<float>(1.1f, 1.6f, 1.4f);
-            genericOperation.FindMax<string>("a", "b", "c");
+
+            
+
+            genericOperation.FindMax<int>(arr);
+            genericOperation.FindMax<float>(Farr);
+            genericOperation.FindMax<string>(Sarr);
         }
     }
 }
