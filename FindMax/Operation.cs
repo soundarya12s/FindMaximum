c35b3de3ad6266a379a961340cfac645e69583c2
S Soundarya <soundarya.s@tvsnext.com>
2023-07-24 13:01:40 +0530

    [Soundarya]Add: Added Operation.cs

---
+++ FindMax/Operation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMax
+{
+    public class Operation
+    {
+
+        public void FindMaxInteger(int a, int b, int c)
+        {
+            if (a >= b && a >= c)
+            {
+                Console.WriteLine("int a is max");
+            }
+            if (b >= a && b >= c)
+            {
+                Console.WriteLine("int b is max");
+            }
+            if (c >= a && c >= b)
+            {
+                Console.WriteLine("int c is max");
+            }
+        }
+        public void FindMaxFloat(float a, float b, float c)
+        {
+            if (a >= b && a >= c)
+            {
+                Console.WriteLine("float a is max");
+            }
+            if (b >= a && b >= c)
+            {
+                Console.WriteLine("float b is max");
+            }
+            if (c >= a && c >= b)
+            {
+                Console.WriteLine("float c is max");
+            }
+        }
+        public void FindMaxString(string a, string b, string c)
+        {
+            if (a.CompareTo(b) >= 0 && a.CompareTo(c) >= 0)
+                Console.WriteLine("string a is max");
+            if (b.CompareTo(a) >= 0 && b.CompareTo(c) >= 0)
+                Console.WriteLine("string b is max");
+            if (c.CompareTo(a) >= 0 && c.CompareTo(b) >= 0)
+                Console.WriteLine("string c is max");
+        }
+    }
+}
---
+++ FindMax/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace FindMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operation operation = new Operation();
+            operation.FindMaxInteger(1, 2, 3);
+            operation.FindMaxFloat(1.2f, 1.6f, 1.1f);
+            operation.FindMaxString("a", "b", "c");
+        }
+    }
+}
